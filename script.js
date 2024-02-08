46ec71f9dd232d1422b519ee7685c47f45586491
Lama-dramala <96291338+Lama-dramala@users.noreply.github.com>
2024-02-08 12:52:40 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -20,6 +20,19 @@ const mainSlider = new Swiper('#main-slider', {
   },
 });
 
+// Старт/пауза автовоспроизведения слайдера
+isSliderPaused = false
+function toggelSliderPause(){
+  if(isSliderPaused){
+    mainSlider.autoplay.start()
+  } else{
+    mainSlider.autoplay.stop()
+  }
+  isSliderPaused = !isSliderPaused
+}
+
+
+
 // Объявление и функционал слайдеров в functions-section
 const functionsSwiper1 = new Swiper('#functions_swiper-1', {
   direction: 'horizontal',
@@ -49,6 +62,8 @@ const functionsSwiper2 = new Swiper('#functions_swiper-2', {
 functionsSwiper1.controller.control = functionsSwiper2;
 functionsSwiper2.controller.control = functionsSwiper1;
 
+
+
 // Объявление и функционал слайдеров в control-section
 const controlSwiper1 = new Swiper('#control_swiper-1', {
   direction: 'horizontal',
@@ -77,3 +92,38 @@ const controlSwiper2 = new Swiper('#control_swiper-2', {
 });
 controlSwiper1.controller.control = controlSwiper2;
 controlSwiper2.controller.control = controlSwiper1;
+
+
+
+// Показ car-info_bottom при скроле вверх
+let ScrollPosition = 0
+const bottomBlock = document.getElementsByClassName("car-info_bottom")[0]
+window.addEventListener('scroll', function() {
+  let current = window.scrollY
+  if (ScrollPosition > current){
+    bottomBlock.classList.add("active")
+  } else{
+    bottomBlock.classList.remove("active")
+  }
+  ScrollPosition = current
+});
+
+
+
+// Переключение цветов в colors-showcase
+let colorsSection = document.getElementsByClassName("colors-showcase")[0].style
+colors = ["linear-gradient(90deg, #BABABA 0%, #F3F3F3 100%)", "black", "green", "aqua", "silver", "brown"]
+let buttons = document.querySelectorAll(".colors_list button")
+
+function changeCarColor(index){  
+  if (index == 0){
+    colorsSection.background = colors[index]
+    colorsSection.color = "#18181B"
+  } else{
+    colorsSection.background = colors[index]
+    colorsSection.color = "white"
+  }
+  buttons.forEach((btn) => btn.classList.remove("active"))
+  buttons[index].classList.add("active")
+}
+
